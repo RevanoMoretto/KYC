e4dfdd42f92af0f36237ae81b8783b4113d3afa2
Moretto <revano.moretto@muf.co.id>
2025-04-09 16:40:25 +0700

    First commit

---
+++ src/containers/home/index.js
@@ -0,0 +1,17 @@
+import classes from './style.module.less';
+
+const propTypes = {};
+const defaultProps = {};
+
+const Index = (props) => {
+	return (
+		<>
+			<label className={classes.wrapper}>Welcome to MASS Screening 3 KYC</label>
+		</>
+	);
+};
+
+Index.propTypes = propTypes;
+Index.defaultProps = defaultProps;
+
+export default Index;
