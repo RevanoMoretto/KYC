e81086185f4d4ba8182621e7cc6a97a13d908040
Moretto <revano.moretto@muf.co.id>
2025-04-22 15:39:10 +0700

    refactor add template component addres

--- src/components/InformasiPekerjaanNasabah/index.jsx
+++ src/components/InformasiPekerjaanNasabah/index.jsx
@@ -1,99 +1,117 @@
 import { Button, Input, Col, Form, Row, Select} from "antd";
 import React from "react";
-import { CaretDownOutlined, SearchOutlined } from "@ant-design/icons";
+import {SearchOutlined } from "@ant-design/icons";
+import UploadImg from "../Helper/UploadImg";
+import classes from './style.module.less';
 
 const InformasiPekerjaanNasabah = ()=>{
+
+    const optionDummy = [
+        {label:"Copet", value:"Copet"},
+        {label:"Begal", value:"Begal"},
+        {label:"Pemuda Pancasila", value:"Pemuda Pancasila"},
+        {label:"Ngepet", value:"Ngepet"},
+    ]
+
+    const handleChangePekerjaanNasabah = (e) => {
+        console.log("test option pekerjaan nasabah: ", e)
+    }
 
     const [form] = Form.useForm();
 
     return (
-        <div>
-            <Form
-				layout="vertical"
-				// onValuesChange={updateOccupation}
-				form={form}
-			>
-                <Row gutter={12}>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Pekerjaan Nasabah">
-                            <Select
-                                disabled={true}
-                                suffixIcon={<CaretDownOutlined />}
-                            />
-                        </Form.Item>
-                    </Col>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Jenis Tempat Bekerja">
-                            <Select
-                                suffixIcon={<CaretDownOutlined />}
-                            />
-                        </Form.Item>
-                    </Col>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Nama Tempat Bekerja">
-                            <Input readOnly
-                            />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={12}>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Jabatan Nasabah">
-                            <Select
-                                suffixIcon={<CaretDownOutlined />}
-                            />
-                        </Form.Item>
-                    </Col>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Sektor Tempat Bekerja">
-                            <Select
-                                suffixIcon={<CaretDownOutlined />}
-                            />
-                        </Form.Item>
-                    </Col>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Sektor Ekonomi">
-                            <Select
-                                suffixIcon={<CaretDownOutlined />}
-                            />
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={12}>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Sektor Ekonomi (Level 2)">
-                            <Select
-                                suffixIcon={<CaretDownOutlined />}
-                            />
-                        </Form.Item>
-                    </Col>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Sektor Ekonomi (Level 1)">
-                            <Select
-                                suffixIcon={<CaretDownOutlined />}
-                            />
-                        </Form.Item>
-                    </Col>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Jarak Tempat Bekerja Nasabah ke MUF">
-                            <Input suffix={<Button padding={0}>KM</Button>}/>
-                        </Form.Item>
-                    </Col>
-                </Row>
-                <Row gutter={12}>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Dokumen Bukti Bekerja (ID Card, Slip Gaji, Suket Kerja/Pengangkatan, dll)">
-                            <Input type="file" name="filename"/>
-                        </Form.Item>
-                    </Col>
-                    <Col md={8} xs={24}>
-                        <Form.Item label="Dokumen Bukti Bekerja (ID Card, Slip Gaji, Suket Kerja/Pengangkatan, dll)">
-                            <Input type="file" name="filename"/>
-                        </Form.Item>
-                    </Col>
-                </Row>
-            </Form>
-        </div>
+        <Form
+            layout="vertical"
+            // onValuesChange={updateOccupation}
+            form={form}
+        >
+            <Row gutter={12}>
+                <Col md={8} xs={24}>
+                    <Form.Item label="Pekerjaan Nasabah">
+                        <Select
+                            placeholder="PILIH PEKERJAAN NASABAH"
+                            onChange={handleChangePekerjaanNasabah}
+                            options={optionDummy}
+                            // suffixIcon={<CaretDownOutlined />}
+                        />
+                    </Form.Item>
+                </Col>
+                <Col md={8} xs={24}>
+                    <Form.Item label="Jenis Tempat Bekerja">
+                        <Select
+                            // suffixIcon={<CaretDownOutlined />}
+                            disabled={true}
+                        />
+                    </Form.Item>
+                </Col>
+                <Col md={8} xs={24}>
+                    <Form.Item 
+                        label="Nama Tempat Bekerja"
+                        name="nama_tempat_kerja_nas"
+                    >
+                        <Input 
+                            readOnly
+                            className={classes.readonly_input_field}
+                        />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row gutter={12}>
+                <Col md={8} xs={24}>
+                    <Form.Item label="Jabatan Nasabah">
+                        <Select
+                            // suffixIcon={<CaretDownOutlined />}
+                        />
+                    </Form.Item>
+                </Col>
+                <Col md={8} xs={24}>
+                    <Form.Item label="Sektor Tempat Bekerja">
+                        <Select
+                        />
+                    </Form.Item>
+                </Col>
+                <Col md={8} xs={24}>
+                    <Form.Item label="Sektor Ekonomi">
+                        <Select
+                            suffixIcon={<SearchOutlined />}
+                        />
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row gutter={12}>
+                <Col md={8} xs={24}>
+                    <Form.Item label="Sektor Ekonomi (Level 2)">
+                        <Select
+                            // suffixIcon={<CaretDownOutlined />}
+                        />
+                    </Form.Item>
+                </Col>
+                <Col md={8} xs={24}>
+                    <Form.Item label="Sektor Ekonomi (Level 1)">
+                        <Select
+                            // suffixIcon={<CaretDownOutlined />}
+                        />
+                    </Form.Item>
+                </Col>
+                <Col md={8} xs={24}>
+                    <Form.Item label="Jarak Tempat Bekerja Nasabah ke MUF">
+                        <Input suffix={<Button padding={0}>KM</Button>}/>
+                    </Form.Item>
+                </Col>
+            </Row>
+            <Row gutter={12}>
+                <Col md={8} xs={24}>
+                    <Form.Item label="Dokumen Bukti Bekerja (ID Card, Slip Gaji, Suket Kerja/Pengangkatan, dll)">
+                        <UploadImg/>
+                    </Form.Item>
+                </Col>
+                <Col md={8} xs={24}>
+                    <Form.Item label="Dokumen Bukti Bekerja (ID Card, Slip Gaji, Suket Kerja/Pengangkatan, dll)">
+                        <UploadImg/>
+                    </Form.Item>
+                </Col>
+            </Row>
+        </Form>
     )
 };
 
