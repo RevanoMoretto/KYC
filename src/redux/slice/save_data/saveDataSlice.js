042448cc96016e3e394b66a8564465a8d163e722
Abdurrahman <abdurrahman.abdurrahman1@muf.co.id>
2025-05-15 11:13:51 +0700

    refactor: perbaikan redux save data

--- src/redux/slice/save_data/saveDataSlice.js
+++ src/redux/slice/save_data/saveDataSlice.js
@@ -32,6 +32,8 @@ const saveDataSlice = createSlice({
     saveData: (state, action) => {
       const { subtab, fields } = action.payload
 
+      const prev = state.formData
+
       if(subtab == "emergency_contact"){
         const { 
           nama_ec,
@@ -53,28 +55,26 @@ const saveDataSlice = createSlice({
           provinsi_ec_desc
         } = fields || {}
 
-        const prev = state.formData.emergency_contact
-
-        state.formData.emergency_contact.nama_emergency_contact = nama_ec ?? prev.nama_emergency_contact
-        state.formData.emergency_contact.nohp1_emergency_contact = nomor_hp_1_ec ?? prev.nohp1_emergency_contact
-        state.formData.emergency_contact.nohp2_emergency_contact = nomor_hp_2_ec ?? prev.nohp2_emergency_contact
-        state.formData.emergency_contact.alamat_emergency_contact = alamat_ec ?? prev.alamat_emergency_contact
-        state.formData.emergency_contact.rt_emergency_contact = rt_ec ?? prev.rt_emergency_contact
-        state.formData.emergency_contact.rw_emergency_contact = rw_ec ?? prev.rw_emergency_contact
-        state.formData.emergency_contact.hubungan_emergency_contact_code = hubungan_ec_code ?? prev.hubungan_emergency_contact_code
-        state.formData.emergency_contact.hubungan_emergency_contact_desc = hubungan_ec_desc ?? prev.hubungan_emergency_contact_desc
-        state.formData.emergency_contact.kodepos_emergency_contact_code = kodepos_ec_code ?? prev.kodepos_emergency_contact_code
-        state.formData.emergency_contact.kelurahan_emergency_contact_code = kelurahan_ec_code ?? prev.kelurahan_emergency_contact_code
-        state.formData.emergency_contact.kelurahan_emergency_contact_desc = kelurahan_ec_desc ?? prev.kelurahan_emergency_contact_desc
-        state.formData.emergency_contact.kecamatan_emergency_contact_code = kecamatan_ec_code ?? prev.kecamatan_emergency_contact_code
-        state.formData.emergency_contact.kecamatan_emergency_contact_desc = kecamatan_ec_desc ?? prev.kecamatan_emergency_contact_desc
-        state.formData.emergency_contact.kabkota_emergency_contact_code = kabkota_ec_code ?? prev.kabkota_emergency_contact_code
-        state.formData.emergency_contact.kabkota_emergency_contact_desc = kabkota_ec_desc ?? prev.kabkota_emergency_contact_desc
-        state.formData.emergency_contact.provinsi_emergency_contact_code = provinsi_ec_code ?? prev.provinsi_emergency_contact_code
-        state.formData.emergency_contact.provinsi_emergency_contact_desc = provinsi_ec_desc ?? prev.provinsi_emergency_contact_desc
+        state.formData.emergency_contact.nama_emergency_contact = nama_ec ?? prev.emergency_contact.nama_emergency_contact
+        state.formData.emergency_contact.nohp1_emergency_contact = nomor_hp_1_ec ?? prev.emergency_contact.nohp1_emergency_contact
+        state.formData.emergency_contact.nohp2_emergency_contact = nomor_hp_2_ec ?? prev.emergency_contact.nohp2_emergency_contact
+        state.formData.emergency_contact.alamat_emergency_contact = alamat_ec ?? prev.emergency_contact.alamat_emergency_contact
+        state.formData.emergency_contact.rt_emergency_contact = rt_ec ?? prev.emergency_contact.rt_emergency_contact
+        state.formData.emergency_contact.rw_emergency_contact = rw_ec ?? prev.emergency_contact.rw_emergency_contact
+        state.formData.emergency_contact.hubungan_emergency_contact_code = hubungan_ec_code ?? prev.emergency_contact.hubungan_emergency_contact_code
+        state.formData.emergency_contact.hubungan_emergency_contact_desc = hubungan_ec_desc ?? prev.emergency_contact.hubungan_emergency_contact_desc
+        state.formData.emergency_contact.kodepos_emergency_contact_code = kodepos_ec_code ?? prev.emergency_contact.kodepos_emergency_contact_code
+        state.formData.emergency_contact.kelurahan_emergency_contact_code = kelurahan_ec_code ?? prev.emergency_contact.kelurahan_emergency_contact_code
+        state.formData.emergency_contact.kelurahan_emergency_contact_desc = kelurahan_ec_desc ?? prev.emergency_contact.kelurahan_emergency_contact_desc
+        state.formData.emergency_contact.kecamatan_emergency_contact_code = kecamatan_ec_code ?? prev.emergency_contact.kecamatan_emergency_contact_code
+        state.formData.emergency_contact.kecamatan_emergency_contact_desc = kecamatan_ec_desc ?? prev.emergency_contact.kecamatan_emergency_contact_desc
+        state.formData.emergency_contact.kabkota_emergency_contact_code = kabkota_ec_code ?? prev.emergency_contact.kabkota_emergency_contact_code
+        state.formData.emergency_contact.kabkota_emergency_contact_desc = kabkota_ec_desc ?? prev.emergency_contact.kabkota_emergency_contact_desc
+        state.formData.emergency_contact.provinsi_emergency_contact_code = provinsi_ec_code ?? prev.emergency_contact.provinsi_emergency_contact_code
+        state.formData.emergency_contact.provinsi_emergency_contact_desc = provinsi_ec_desc ?? prev.emergency_contact.provinsi_emergency_contact_desc
       }
 
-      state.formData.hasil_kyc = fields.hasil_kyc_input
+      state.formData.hasil_kyc = fields.hasil_kyc_input ?? prev.hasil_kyc
     }
   }
 });
